4a33c76ace4c4c42467857a15a3472a55a44ceb4
madja0125 <dejesusmiguel788@gmail.com>
2024-01-24 20:53:14 +0000

    adding 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
 def islower(c):
-    if c >= chr(97) and c <= chr(122):
-        return True
-    elif c >= chr(65) and c <= chr(90):
-        return False
+    if ord(c) >= 97 and ord(c) <= 122:
+        return (True)
     else:
-        return
+        return (False)
