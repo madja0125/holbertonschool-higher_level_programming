a75f7fc2e9212f518061dc4c9a4576447aca8670
madja0125 <dejesusmiguel788@gmail.com>
2024-01-27 02:38:33 +0000

    adding task 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+
+    if my_list is None:
+        return None
+
+    if len(my_list) == 0:
+        return None
+
+    max_integer = sorted(my_list, reverse=True)
+
+    return max_integer[0]
