762cee432a8d89e70fd1c46a2d1b4357e71bb661
madja0125 <dejesusmiguel788@gmail.com>
2024-01-29 19:35:19 +0000

    changing to lambda

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,16 +2,6 @@
 
 def search_replace(my_list, search, replace):
 
-    if my_list is None:
-        return
-
-    if len(my_list) == 0:
-        return
-
-    new_list = my_list.copy()
-
-    for i in range(len(new_list)):
-            if new_list[i] == search:
-                new_list[i] = replace
-
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
     return new_list
+
