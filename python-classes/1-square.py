6ce2f9c6970f29fa80bade565b9ad77551a60f76
madja0125 <dejesusmiguel788@gmail.com>
2024-02-03 00:46:58 +0000

    adding task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Module: 1-square
+class Square
+"""
+
+
+class Square:
+    """
+    class Square
+    """
+
+    def __init__(self, size):
+        """
+        Size is the size of the square
+        """
+        self.__size = size
