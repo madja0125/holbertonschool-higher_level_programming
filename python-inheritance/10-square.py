75edc9ca99e1e01d4478ee31cf5e4236e9f59ec7
madja0125 <dejesusmiguel788@gmail.com>
2024-02-12 03:07:46 +0000

    fixing 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,54 +1,17 @@
 #!/usr/bin/python3
 
-"""Module that have a class that inherits from other one"""
 
-
-class BaseGeometry:
-    """Class that have the method"""
-
-    def area(self):
-        """method that raise en message error"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """method that validates value"""
-        if isinstance(name, str):
-            if type(value) is not int:
-                raise TypeError(f"{name} must be an integer")
-            if value <= 0:
-                raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    """class inherited from Basegeometry"""
-
-    def __init__(self, width, height):
-        """Method that set width and height private and
-        validate that they are positive integers"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """method that returns the area of the instance"""
-        return self.__height * self.__width
-
-    def __str__(self):
-        """method that returns the printable string"""
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ class inherited from Rectangle"""
-
+    """ Class that defines a Square from Rectangle class """
     def __init__(self, size):
-        """method that set size private and validate if is
-        a positive integer"""
+        """ Method that initializes a Square """
         self.integer_validator("size", size)
         self.__size = size
-        super().__init__(size, size)
+        super().__init__(self.__size, self.__size)
 
     def area(self):
-        """method that returns the area of the instance"""
-        return self.__height * self.__width
+        """ Method that returns a string with the area """
+        return super().area()
