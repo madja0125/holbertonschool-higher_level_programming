7c88378e014de267f4708e7107a17b753a5a4abb
madja0125 <dejesusmiguel788@gmail.com>
2024-02-12 03:12:39 +0000

    fixing

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-Rectangle = __import__('9-rectangle').Rectangle
+class Rectangle(BaseGeometry):
+    """ Class that defines a rectangle from BaseGeometry Class """
 
-
-class Square(Rectangle):
-    """ Class that defines a Square from Rectangle class """
-    def __init__(self, size):
-        """ Method that initializes a Square """
-        self.integer_validator("size", size)
-        self.__size = size
-        super().__init__(self.__size, self.__size)
+    def __init__(self, width, height):
+        """ Initializes instance """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
     def area(self):
-        """ Method that returns a string with the area """
-        return super().area()
+        """ Method that returns the area of the instance"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ Special method that returns the printable string """
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
