7e00b51ed9a90901a0b60ed391618c9523529bae
madja0125 <dejesusmiguel788@gmail.com>
2024-02-12 03:09:05 +0000

    fixing

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,39 +1,23 @@
 #!/usr/bin/python3
 
-"""Module that have a class that inherits from other one"""
 
-
-class BaseGeometry:
-    """Class that have the method"""
-
-    def area(self):
-        """method that raise en message error"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """method that validates value"""
-        if isinstance(name, str):
-            if type(value) is not int:
-                raise TypeError(f"{name} must be an integer")
-            if value <= 0:
-                raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """class inherited from Basegeometry"""
+    """ Class that defines a rectangle from BaseGeometry Class """
 
     def __init__(self, width, height):
-        """Method that set width and height private and
-        validate that they are positive integers"""
+        """ Initializes instance """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
 
     def area(self):
-        """method that returns the area of the instance"""
-        return self.__height * self.__width
+        """ Method that returns the area of the instance"""
+        return self.__width * self.__height
 
     def __str__(self):
-        """method that returns the printable string"""
-        return f"[Rectangle] {self.__width}/{self.__height}"
+        """ Special method that returns the printable string """
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
