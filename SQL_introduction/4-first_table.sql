fbf8b4eccffa3481ff7585911f6c63726c67b934
madja0125 <dejesusmiguel788@gmail.com>
2024-03-27 08:27:27 +0000

    fixing

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,5 @@
--- CREATING A TABLE
-
+-- Creates a table in the current database
+-- Query to create a table called first_table in the current database in your MySQL server
 CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-);
+id INT,
+name VARCHAR(256));
