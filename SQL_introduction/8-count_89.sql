dfa561325f6931fcb2637810a2d29f36fa4a8a25
madja0125 <dejesusmiguel788@gmail.com>
2024-03-27 08:29:18 +0000

    fixing

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
--- displays the number of records with id = 89 in the table first_table
-
-SELECT COUNT(*) FROM first_table WHERE id=89;
+-- Displays the number of records
+-- Query to display the number of records with id = 89 in a table
+SELECT COUNT(*) as id FROM first_table WHERE id=89;
