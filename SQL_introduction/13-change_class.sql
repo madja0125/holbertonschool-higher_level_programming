a2f69f6fffe36395c057724c4bebd7ab1ee431e4
madja0125 <dejesusmiguel788@gmail.com>
2024-03-27 08:34:35 +0000

    fixing

--- SQL_introduction/13-change_class.sql
+++ SQL_introduction/13-change_class.sql
@@ -1,4 +1,3 @@
--- removing all records with a score <= 5 in the table second_table
-
-DELETE FROM second_table
-WHERE score <= 5;
+-- Script that removes records
+-- Query to remove all records with a score <= 5 in the table second_table
+DELETE FROM second_table WHERE score <= 5;
