b5ef774ab4fe8eade61af23ab65248052c435292
madja0125 <dejesusmiguel788@gmail.com>
2024-03-27 08:33:49 +0000

    fixing

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,5 @@
--- Updating the score of Bob to 10
-
+-- Script that updates the attribute value of a table
+-- Query to update the score of Bob to 10 in the table second_table
 UPDATE second_table
 SET score = 10
-WHERE name = 'Bob';
+WHERE name = "Bob"
