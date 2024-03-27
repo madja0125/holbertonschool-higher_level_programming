2f0be98f4fdcdf5db84ba96530345b6b8c65f619
madja0125 <dejesusmiguel788@gmail.com>
2024-03-27 23:15:23 +0000

    fixing

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,5 @@
--- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
-
-SELECT tv_shows.title, tv_show_genres.genre_id
+-- Script that lists all cities contained in the database
+SELECT tv_shows.title, tv_show_genres.genre_id -- Query to join cities and states
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
